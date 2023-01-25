99694c1af8bc0f117354892bf23f7298d834b774
valelagrutta <123384072+valelagrutta@users.noreply.github.com>
2023-01-25 15:57:52 -0300

    Add files via upload

---
+++ java para cv.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').onclick= function(){
+    console.log ("mostrar datos");
+    document.getElementById('boton').innerHTML = "Fecha de Nacimiento: 4/7/1980, Soltera, CUIT:27-28112276-8, Monotributista, Dir: Urquiza 968 Dto 6 Ramos M, Buenos Aires";
+
+}
